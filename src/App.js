04516309b9c2f113941988df1f0ccb21684c7ead
P4duk3s <zygisbalcius@gmail.com>
2021-03-29 01:22:53 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '', 
+      list: []
+    };
+  }
+
+  onChangeValue = e => {
+    this.setState({value: e.target.value});
+  };
+
+  onAddItem = () => {
+    this.setState(state => {
+      const list = [state.value, ...state.list]
+      return {
+        value: '',
+        list
+      };
+    });
+  };
+
+  onRemoveItem = i => {
+    this.setState(state => {
+      const list = state.list.filter((item, j) => i !== j);
+ 
+      return {
+        list,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="app-title">TO DO LIST</h1>
+        <div className="container">
+        <input
+          type="text"
+          placeholder="Give me a task!"
+          onChange={this.onChangeValue} 
+          value={this.state.value}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </input>
+        <button
+          className="add-btn"
+          type="button"
+          onClick={this.onAddItem}
+          disabled={!this.state.value.length}
+        >
+          +
+        </button>
+        <ul>
+          {this.state.list.map((item, index) => {
+            return(
+              <li 
+                key={item}>{item}
+              <button
+                className="btn"
+                onClick={() => this.onRemoveItem(index)}
+              >X</button></li>
+            )
+          })}
+        </ul>
+      </div>
+      </div>
+      
+      
+        
+    );
+  }
 }
 
 export default App;
